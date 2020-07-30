c26aeab954c4f5cb46d47bbf96d52c06174809a8
Your NameSuyanne <suyanne.gabriele@gmail.com>
2020-07-30 20:38:23 -0300

    trabalhando com o java - básico

---
+++ exemplo-logica/CalculaIdade.java
@@ -0,0 +1,19 @@
+public class CalculaIdade{
+
+	public static void main(String[] args){
+		int anoAtual;
+		int idade;
+		int anoNascimento;
+
+		anoAtual = 2020;
+		anoNascimento = 2000;
+
+		idade = anoAtual - anoNascimento;
+
+		System.out.println("A idade do usuário e " + idade );
+		
+		
+
+}
+
+}
---
+++ exemplo-logica/UsandoTexto.java
@@ -0,0 +1,11 @@
+public class UsandoTexto{
+
+	public static void main(String[] args){
+		String nome;
+
+		nome = "Suy";		
+
+	System.out.println("Meu nome e " + nome);
+}
+
+}
