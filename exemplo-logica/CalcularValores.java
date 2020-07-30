d6e9f7ef6953d7b5810ee76ed807f4c561aea0d1
Your NameSuyanne <suyanne.gabriele@gmail.com>
2020-07-30 20:48:02 -0300

    calculos

---
+++ exemplo-logica/CalcularValores.java
@@ -0,0 +1,13 @@
+public class CalcularValores{
+
+	public static void main(String[] args){
+		String nome;
+        double altura;
+
+        nome = "Suyanne";
+        altura = 1.64;
+
+        System.out.println("Oiie, meu nome é " + nome + "! E eu tenho " + altura + " metros");
+}
+
+}
