8c5d07488856d8b2b58eeec3b42dc2789fa39329
Your NameSuyanne <suyanne.gabriele@gmail.com>
2020-07-30 21:10:01 -0300

    finalizando os calculos

--- exemplo-logica/CalcularValores.java
+++ exemplo-logica/CalcularValores.java
@@ -2,17 +2,24 @@ public class CalcularValores{
 
 	public static void main(String[] args){
 
-        int numeroUm;
-        int numeroDois;
-        int soma;
+        double numeroUm;
+        double numeroDois;
+        double soma, subtracao, divisao, multiplicacao;
+
 
         // Irá fazer um calculo de soma
         numeroUm = 15;
         numeroDois = 80;
 
         soma = numeroUm + numeroDois;
+        subtracao = numeroUm - numeroDois;
+        divisao = numeroUm / numeroDois;
+        multiplicacao = numeroUm * numeroDois;
         
-        System.out.println("O resultado: " + soma);
+        System.out.println("Resultado da soma: " + soma);
+        System.out.println("Resultado da subtracao: " + subtracao);
+        System.out.println("Resultado da divisao: " + divisao);
+        System.out.println("Resultado da multipicacao: " + multiplicacao);
 }
 
 }
